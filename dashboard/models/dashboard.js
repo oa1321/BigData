6eea0d6dae180d8854195ae23d55d7d6fabda93a
oa1321 <oalon52@gmail.com>
2023-03-16 19:19:32 +0200

    dashboard get data fully from redis now

--- dashboard/models/dashboard.js
+++ dashboard/models/dashboard.js
@@ -6,16 +6,96 @@ async function start_seq(){
 }
 start_seq()
 async function get_top5_add(){
-    return {"זייתים":30, "תירס": 20, "פטריות": 10, "אנשובי": 5, "בצל":5};
+    //lets assume we have 8 kinds of toppings
+    //olives
+    //corn
+    //mushrooms
+    //anshoby
+    //onions
+    //cheese
+    //peppers
+    //tomato
+    
+    const top1 = await client.get('olives')
+    const top2 = await client.get('corn')
+    const top3 = await client.get('mushrooms')
+    const top4 = await client.get('anshoby')
+    const top5 = await client.get('onions')
+    const top6 = await client.get('cheese')
+    const top7 = await client.get('peppers')
+    const top8 = await client.get('tomato')
+    tops = [[top1,'olives'],[top2,'corn'],[top3,'mushrooms'],[top4,'anshoby'],[top5,'onions'],[top6,'cheese'],[top7,'peppers'],[top8,'tomato']]
+    tops.sort((a, b) => { 
+        
+    if (parseInt(a[0]) > parseInt(b[0]) ){
+        return -1
+    }
+    if (parseInt(a[0]) < parseInt(b[0]) ){
+        return 1
+    }
+    return 0 
+    } )
+    var dict = {}
+    dict[tops[0][1]] = tops[0][0]
+    dict[tops[1][1]] = tops[1][0]
+    dict[tops[2][1]] = tops[2][0]
+    dict[tops[3][1]] = tops[3][0]
+    dict[tops[4][1]] = tops[4][0]
+    return dict;
 }
 async function get_top5_city(){
-    return {"חיפה":30, "אילת": 34, "תל אביב": 40, "יהוד": 55, "אריאל":55};
+    const cities = ["Jerusalem", "TelAviv", "Haifa", "RishonLeZion",
+     "PetahTikva", "Ashdod", "Netanya", "BeerSheva", "BneiBrak",
+      "Holon", "BatYam", "RamatGan", "Rehovot", "Herzliya", "KfarSaba", "Nahariya", "Modi'in-Maccabim-Re'"]; 
+    // Define an empty array to store the city-value pairs
+    const cityValues = [];
+
+    // Loop through each city name in the array
+    for (const city of cities) {
+        // Get the value for the city using the Redis client
+        var x = await client.get(city);
+        // Push the city-value pair to the array
+        cityValues.push([city, x]);
+    }
+    // Sort the array by the second value in the pair (ascending order)
+    cityValues.sort((a, b) => parseInt(a[1]) - parseInt(b[1]));
+    // Define an empty object to store the city-value pairs
+    const lowestCityValues = {};
+
+    // Loop through the first 5 elements in the sorted cityValues array
+    for (let i = 0; i < Math.min(5, cityValues.length); i++) {
+    // Get the city name and value from the current element
+    const [city, value] = cityValues[i];
+    
+    // Add the city-value pair to the lowestCityValues object
+    lowestCityValues[city] = value.toString();
+    }
+    // Print the lowestCityValues object to the console
+    return lowestCityValues;
 }
 async function get_regins(){
-    return {"חיפה":30, "דן": 34, "מרכז": 40, "צפון": 55, "דרום":55};
+    x1 = await client.get("חיפה")
+    x2 = await client.get("דן")
+    x3 = await client.get("מרכז")
+    x4 = await client.get("צפון")
+    x5 = await client.get("דרום")
+    return {"חיפה":parseInt(x1),
+     "דן": parseInt(x2),
+      "מרכז":parseInt(x3),
+       "צפון": parseInt(x4),
+        "דרום": parseInt(x5)};
 }
 async function get_times_today(){
-    return {"10:00":4, "12:00": 12, "14:00": 40, "16:00": 32, "18:00":5};
+    x1 = await client.get("10:00")
+    x2 = await client.get("12:00")
+    x3 = await client.get("14:00")
+    x4 = await client.get("16:00")
+    x5 = await client.get("18:00")
+    return {"10:00":parseInt(x1),
+     "12:00": parseInt(x2),
+      "14:00":parseInt(x3),
+       "16:00": parseInt(x4),
+        "18:00": parseInt(x5)};
 }
 
 async function get_today_amount(){
--- redis/redis_producer.js
+++ redis/redis_producer.js
@@ -9,21 +9,85 @@ async function start_seq(){
     await client.connect()
 }
 start_seq()
+
+async function gen_ramdom_data(req,res){
+  const orders_today = Math.floor(Math.random() * 300);
+  const open_orders = Math.floor(Math.random() * orders_today);
+  const open_stores = Math.floor(Math.random() * 30);
+  const avarage_time = Math.floor(Math.random() * 40);
+  client.set('orders_today', orders_today)
+  client.set('open_orders', open_orders)
+  client.set('open_stores', open_stores)
+  client.set('avarage_time', avarage_time)
+  var d = new Date();
+  var n = d.toLocaleTimeString();
+  client.set('update_time', n)
+
+  //sets tops amount
+  //lets assume we have 8 kinds of toppings
+  //olives
+  //corn
+  //mushrooms
+  //anshoby
+  //onions
+  //cheese
+  //peppers
+  //tomato
+  RAND = 30
+  client.set('olives', Math.floor(Math.random() * RAND))
+  client.set('corn', Math.floor(Math.random() * RAND))
+  client.set('mushrooms', Math.floor(Math.random() * RAND))
+  client.set('anshoby', Math.floor(Math.random() * RAND))
+  client.set('onions', Math.floor(Math.random() * RAND))
+  client.set('cheese', Math.floor(Math.random() * RAND))
+  client.set('cheese', Math.floor(Math.random() * RAND))
+  client.set('peppers', Math.floor(Math.random() * RAND))
+  client.set('tomato', Math.floor(Math.random() * RAND))
+
+
+  // cities
+  const cities = ["Jerusalem", "TelAviv", "Haifa", "RishonLeZion",
+  "PetahTikva", "Ashdod", "Netanya", "BeerSheva", "BneiBrak",
+   "Holon", "BatYam", "RamatGan", "Rehovot", "Herzliya", "KfarSaba", "Nahariya", "Modi'in-Maccabim-Re'"]; 
+
+  // Loop through each city name in the array
+  for (const city of cities) {
+    // Generate a random number between 1 to 60
+    const randomNum = Math.floor(Math.random() * 60) + 10;
+    
+    // Set the random number for the city using the Redis client
+    client.set(city, randomNum);
+    }
+
+
+  //regins
+  const regins = ["חיפה", "דן", "מרכז", "צפון", "דרום"]; 
+
+  // Loop through each city name in the array
+  for (const regin of regins) {
+    // Generate a random number between 1 to 60
+    const randomNum = Math.floor(Math.random() * 120) + 10;
+    
+    // Set the random number for the city using the Redis client
+    client.set(regin, randomNum);
+    }
+
+   //times
+   const times = ["10:00", "12:00", "14:00", "16:00", "18:00"]; 
+
+   // Loop through each city name in the array
+   for (const t of times) {
+     // Generate a random number between 1 to 60
+     const randomNum = Math.floor(Math.random() * 45) + 5;
+     
+     // Set the random number for the city using the Redis client
+     client.set(t, randomNum);
+     }
+
+  res.send(`REDIS GENERATE: ${n} \norders_today set to ${orders_today}\norders_today set to ${open_orders}\norders_today set to ${open_stores}\norders_today set to ${avarage_time}\n`);
+}
 // create an endpoint to generate a random value and save it to Redis
-app.get('/gen', (req, res) => {
-    const orders_today = Math.floor(Math.random() * 300);
-    const open_orders = Math.floor(Math.random() * orders_today);
-    const open_stores = Math.floor(Math.random() * 30);
-    const avarage_time = Math.floor(Math.random() * 40);
-    client.set('orders_today', orders_today)
-    client.set('open_orders', open_orders)
-    client.set('open_stores', open_stores)
-    client.set('avarage_time', avarage_time)
-    var d = new Date();
-    var n = d.toLocaleTimeString();
-    client.set('update_time', n)
-    res.send(`REDIS GENERATE: ${n} \norders_today set to ${orders_today}\norders_today set to ${open_orders}\norders_today set to ${open_stores}\norders_today set to ${avarage_time}\n`);
-  });
+app.get('/gen', (req, res) => {gen_ramdom_data(req,res)});
 
 // create an endpoint to stop the app
 app.get('/stop', (req, res) => {
