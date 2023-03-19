934590c403a25c19ff5bb9fd64704934cbf2d51d
oa1321 <oalon52@gmail.com>
2023-03-19 03:36:28 +0200

    ejs bigml connection

--- dashboard/controllers/analyze.js
+++ dashboard/controllers/analyze.js
@@ -7,9 +7,10 @@ const get_main_page = async (req, res) =>{
 }
 const get_main_page_parms = async (req, res) =>{
     const { date1, date2 } = req.params;
-    console.log("here")
+    console.log("here2")
     console.log(analyze_bigml)
     //shood use the dates to create data.csv
+    //in models i shood create a mongodb readr that saves the data to data.csv
     res.json(await analyze_bigml());
 }
 module.exports = {get_main_page, get_main_page_parms};
